8652ff8ccc04a908b5b995a824eb643a79d359cd
Kimmo Brunfeldt <kimmo.brunfeldt@futurice.com>
2014-01-21 14:09:21 +0200

    Try to fix video

--- js/main.js
+++ js/main.js
@@ -1,19 +1,12 @@
-var screen = new Screen();
+var screenId = 'screen-id-' + ((Math.random() * new Date().getTime()).toString(36).toLowerCase().replace(/\./g, '-'));
+var screen = new Screen(screenId);
+
 
 var screensPreview = document.getElementById('screens-preview');
 
 // on getting each new screen
 screen.onaddstream = function(e) {
-    var hash = window.location.hash.replace('#', '');
-    if (!hash.length) location.href = location.href + '#screen-id-' + ((Math.random() * new Date().getTime()).toString(36).toLowerCase().replace(/\./g, '-'));
-
-    var button = document.createElement('button');
-
-    $('#stop-screen-share').on('click', function() {
-        screen.leave();
-        this.disabled = true;
-        $('.screen-preview').show();
-    });
+    location.href = location.href + '#' + screenId;
 
     screensPreview.appendChild(document.createElement('hr'));
     screensPreview.appendChild(e.video);
@@ -32,9 +25,12 @@ screen.onuserleft = function(userid) {
 // check pre-shared screens
 screen.check();
 
-document.getElementById('share-screen').onclick = function() {
+$('#share-screen').on('click', function() {
     screen.share();
-    this.disabled = true;
-
     $('.screen-preview').show();
-};
+});
+
+$('#stop-screen-share').on('click', function() {
+    $('.screen-preview').hide();
+    screen.leave();
+});
